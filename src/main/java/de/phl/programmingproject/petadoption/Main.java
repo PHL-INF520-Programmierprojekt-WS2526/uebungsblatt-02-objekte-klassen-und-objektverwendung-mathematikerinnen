6136dc70bd048f4077928d2b6b84e4e541a5d04f
kubra0666 <kubrakok4266@gmail.com>
2025-11-04 16:08:35 +0100

    Main.java erledigt

--- src/main/java/de/phl/programmingproject/petadoption/Main.java
+++ src/main/java/de/phl/programmingproject/petadoption/Main.java
@@ -7,6 +7,19 @@ import java.util.Date;
  */
 public class Main {
     public static void main(String[] args) {
-        // TODO implement your program here
-    }
-}
+    
+               Pet buddy = new Pet("Buddy", 3, "Labrador Retriever");
+                Adopter jane = new Adopter("Jane Smith", "555-555-5555", "janesmith@email.com");
+        
+                System.out.println("Haustier-Info: " + buddy.getInfo());
+                System.out.println("Adoptierende Person-Info: " + jane.getInfo());
+                System.out.println("Ist Buddy adoptiert? " + buddy.isAdopted());
+        
+                Date today = new Date();
+                Adoption adoption = buddy.adopt(jane, today);
+        
+                System.out.println("Adoption-Info: " + adoption.getInfo());
+                System.out.println("Buddy ist schon adoptiert" + buddy.isAdopted());
+            
+        
+                  }      }
